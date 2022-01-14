230c04c34911d00fbab9c2e7a1a8c4928226d396
ayumitk <ayumee528@gmail.com>
2022-01-14 02:39:22 -0800

    fix: heroセクションの文字サイズを調整

--- gulpfile.js
+++ gulpfile.js
@@ -33,8 +33,8 @@ function browsersync() {
     server: {
       baseDir: "./docs",
     },
-    // open: "external",
-    // host: "192.168.0.24",
+    open: "external",
+    host: "192.168.0.20",
   });
 }
 
